6005e2d02ef907020ba4268c42cd7ec7afb24452
Trinh Duy <khacduyy97@gmail.com>
2019-07-07 16:29:23 -0400

    Add error handler for users route

--- routers/api/users.js
+++ routers/api/users.js
@@ -21,33 +21,38 @@ router.post(
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
-    const errors = await validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { name, email, password } = req.body;
+
+      // Check if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already existed" }] });
+      }
+
+      // Get avatar for user
+      const avatar = await gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+
+      // Encrypt user's password
+      user = new User({ name, email, password, avatar });
+      const salt = await bcrypt.genSalt();
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // Return Json Web Token
+
+      console.log(req.body);
+      res.send("User Registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
     }
-    const { name, email, password } = req.body;
-
-    // Check if user exists
-    let user = await User.findOne({ email });
-    if (user) {
-      return res
-        .status(400)
-        .json({ errors: [{ msg: "User already existed" }] });
-    }
-
-    // Get avatar for user
-    const avatar = await gravatar.url(email, { s: "200", r: "pg", d: "mm" });
-
-    // Encrypt user's password
-    user = new User({ name, email, password, avatar });
-    const salt = await bcrypt.genSalt();
-    user.password = await bcrypt.hash(password, salt);
-    await user.save();
-
-    // Return Json Web Token
-
-    console.log(req.body);
-    res.send("User Registered");
   }
 );
 
