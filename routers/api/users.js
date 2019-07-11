a6a2fa39388f884c1fe30929c5916d0e2a8f5e4d
Trinh Duy <khacduyy97@gmail.com>
2019-07-10 21:22:24 -0400

    Add jwt for users route

--- routers/api/users.js
+++ routers/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require("express-validator");
 const User = require("../../config/models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 // @route   POST api/users
 // @desc    Register user
@@ -45,10 +47,22 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
-      // Return Json Web Token
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
 
-      console.log(req.body);
-      res.send("User Registered");
+      // Return Json Web Token
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
